2170904fce19b60b33718823c65af3d9fcf8600f
1215380 <abhijeet.pathare@infosys.com>
2022-11-22 12:25:57 +0530

    Custom validation in GlobalExceptionHandler class

--- SpringBootDemo01-Udemy-1-Blog/src/main/java/com/app/blog/exceptions/GlobalExceptionHandler.java
+++ SpringBootDemo01-Udemy-1-Blog/src/main/java/com/app/blog/exceptions/GlobalExceptionHandler.java
@@ -1,17 +1,23 @@
 package com.app.blog.exceptions;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.app.blog.payload.ErrorDetails;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	//handle specific exceptions 
 	@ExceptionHandler(ResourceNotFoundException.class)
@@ -40,4 +46,35 @@ public class GlobalExceptionHandler {
 		
 	}
 	
+	//method to get the errors added in validation 
+	//below method is overridden from class ResponseEntityExceptionHandler
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		//create a map object to map the error fields and error-message 
+		Map<String,String> errors = new HashMap<>();
+		
+		//mapping field errors and messages and inserting them into the map 
+		ex.getBindingResult().getAllErrors().forEach((error)-> {
+			String fieldName = ((FieldError)error).getField();
+			String message = error.getDefaultMessage();
+			errors.put(fieldName, message);
+		});
+		return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+	}
+	
+	//BELOW METHOD CAN ALSO BE USED INSTEAD OF ABOVE handleMethodArgumentNotValid METHOD
+//	@ExceptionHandler(MethodArgumentNotValidException.class)
+//	public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest webRequest){
+//		
+//		Map<String,String> errors = new HashMap<>();
+//		exception.getBindingResult().getAllErrors().forEach((error)-> {
+//			String fieldName = ((FieldError)error).getField();
+//			String message = error.getDefaultMessage();
+//			errors.put(fieldName, message);
+//		});
+//		return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+//		
+//	}
 }
